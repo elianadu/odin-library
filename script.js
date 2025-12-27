0942d1edb8a0625fdbec6df69fa4e1fe29054864
elianadu <148028744+elianadu@users.noreply.github.com>
2025-12-27 16:00:46 -0500

    fix: complete library project

---
+++ script.js
@@ -0,0 +1,122 @@
+const myLibrary = [];
+const bookshelf = document.querySelector(".bookshelf");
+
+// tester books
+const songOfAchilles = new Book(
+  "Song of Achilles",
+  "Madeleine Miller",
+  200,
+  true
+);
+const thePhantomTollBooth = new Book(
+  "The Phantom Tollbooth",
+  "Norton Juster",
+  150,
+  false
+);
+
+myLibrary.push(songOfAchilles);
+myLibrary.push(thePhantomTollBooth);
+
+// variables to add new book
+const newBookBtn = document.querySelector("#newBookBtn");
+const dialog = document.querySelector("dialog");
+const confirmBtn = document.querySelector("#confirmBtn");
+
+// show dialog on click
+newBookBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const numPages = document.querySelector("#numPages").value;
+  const hasBeenRead = document.querySelector("#hasBeenRead").checked;
+  addBookToLibrary(title, author, numPages, hasBeenRead);
+  console.log(myLibrary);
+
+  displayBooks();
+  dialog.close();
+});
+
+function Book(title, author, numPages, hasBeenRead) {
+  if (!new.target)
+    throw Error("You must use the 'new' operator to call the constructor.");
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.hasBeenRead = hasBeenRead;
+  this.info = function () {
+    if (this.hasBeenRead) {
+      return `${this.title} by ${this.author}, ${this.numPages} pages, read`;
+    } else {
+      return `${this.title} by ${this.author}, ${this.numPages} pages, not read yet`;
+    }
+  };
+}
+
+function addBookToLibrary(title, author, numPages, hasBeenRead) {
+  // take params, create a book then store it in the array
+  let book = new Book(title, author, numPages, hasBeenRead);
+  myLibrary.push(book);
+}
+
+function removeBookFromLibrary(id) {
+  const index = myLibrary.findIndex((book) => book.id === id);
+  if (index !== -1) {
+    myLibrary.splice(index, 1);
+  }
+  console.log(myLibrary);
+  displayBooks();
+}
+
+function displayBooks() {
+  bookshelf.replaceChildren(); // kind of janky; clear page and display everything again
+  for (let book of myLibrary) {
+    const newBook = document.createElement("div");
+    newBook.classList.add("shelved");
+
+    const bookInfo = document.createElement("div");
+    bookInfo.textContent = book.info();
+
+    // remove book button
+    const removeBookBtn = document.createElement("button");
+    removeBookBtn.textContent = "Remove";
+    removeBookBtn.addEventListener("click", () => {
+      removeBookFromLibrary(book.id);
+    });
+
+    // has been read checkbox
+    const hasBeenReadLabel = document.createElement("label");
+    hasBeenReadLabel.textContent = "Read:";
+    const hasBeenReadBox = document.createElement("input");
+    hasBeenReadBox.type = "checkbox";
+    if (book.hasBeenRead) {
+      hasBeenReadBox.checked = true;
+    }
+
+    hasBeenReadBox.addEventListener("change", () => {
+      book.hasBeenRead = !book.hasBeenRead;
+      bookInfo.textContent = book.info();
+    });
+
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("btnContainer");
+
+    newBook.appendChild(bookInfo);
+
+    hasBeenReadLabel.appendChild(hasBeenReadBox);
+    btnContainer.appendChild(hasBeenReadLabel);
+
+    btnContainer.appendChild(removeBookBtn);
+
+    newBook.appendChild(btnContainer);
+
+    bookshelf.appendChild(newBook);
+  }
+}
+
+displayBooks();
